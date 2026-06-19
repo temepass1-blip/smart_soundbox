122e82143776adf6e6aec1e07f9beaee0301b7a4
temepass1-blip <temepass1@gmail.com>
2026-06-19 16:25:10 +0530

    Fix Android namespace error for deprecated telephony plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,28 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.hasProperty("android")) {
+            try {
+                val androidExt = project.extensions.findByName("android") as? com.android.build.gradle.LibraryExtension
+                if (androidExt != null && androidExt.namespace == null) {
+                    val manifestFile = project.file("src/main/AndroidManifest.xml")
+                    if (manifestFile.exists()) {
+                        val content = manifestFile.readText()
+                        val matcher = java.util.regex.Pattern.compile("package=\"([^\"]+)\"").matcher(content)
+                        if (matcher.find()) {
+                            androidExt.namespace = matcher.group(1)
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                // Ignore
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
